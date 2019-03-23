e22f48ab34976ad6ecc1822cb3e5b4dd0ffb8a5c
coconut-irl <coconutx206@gmail.com>
2019-03-23 11:23:33 +0530

    Initial commit

---
+++ who-goes-there.py
@@ -0,0 +1,14 @@
+# Write a program that gets 2 string variables and 2 number variables from the user, 
+# concatenates (joins them together with no spaces) and displays the strings,
+# then multiplies the two numbers on a new line.
+
+string1 = input("Enter a string: ")
+string2 = input("Enter another string: ")
+print()
+float1 = float(input("Enter a number: "))
+float2 = float(input("Enter another number: "))
+print()
+print("Concatenated string: ", string1 + string2)
+print()
+
+print("Multiplication of the two numbers results in: ", float1 * float2)
